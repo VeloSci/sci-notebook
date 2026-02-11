e081ca16b6279f7a5e305fdf7644d5c60262a0cc
Jose Ivan <jigonzalez930209@gmail.com>
2026-02-10 23:04:14 -0300

    docs: add notebooks index

---
+++ docs/.vitepress/theme/notebooks/index.ts
@@ -0,0 +1,8 @@
+export { overviewNotebook } from './overview';
+export { cellTypesNotebook } from './cell-types';
+export { mathEditorNotebook } from './math-editor';
+export { presentationModeNotebook } from './presentation-mode';
+export { exportPluginsNotebook } from './export-plugins';
+export { cloudSyncNotebook } from './cloud-sync';
+export { mobileSupportNotebook } from './mobile-support';
+export { frameworkAdaptersNotebook } from './framework-adapters';
