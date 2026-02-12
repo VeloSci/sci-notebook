33ddba1c9d7f136944fd635c372e9af27fa71d5e
Jose Ivan <jigonzalez930209@gmail.com>
2026-02-11 22:44:40 -0300

    chore(svelte): update public API

--- packages/svelte/src/index.ts
+++ packages/svelte/src/index.ts
@@ -2,3 +2,36 @@ export { SciNotebookSvelte } from "./sci-notebook-svelte";
 export type { SciNotebookSvelteOptions } from "./sci-notebook-svelte";
 export { createNotebookStore } from "./stores";
 export type { NotebookStore } from "./stores";
+export { MATH_CATEGORIES } from "./math-categories";
+export type { MathBlock, MathCategory } from "./math-categories";
+export {
+  FloatingToolbar,
+  SlashCommandMenu,
+  DEFAULT_COMMANDS,
+  buildTOCItems,
+  createTOCSidebar,
+  FindReplaceBar,
+  renderCellOutput,
+  renderCellOutputs,
+  renderImagePreview,
+  renderEmbedPreview,
+  renderTablePreview,
+  renderMermaidToSvg,
+  initMermaid,
+  LatexAutocompleteMenu,
+  LATEX_COMMANDS,
+  ChatSidebarPanel,
+  AIRewritePanel,
+  AICellGeneratePanel,
+  GhostTextOverlay,
+  ImageResizeHandle,
+} from "./components";
+export type {
+  SlashCommandItem,
+  TOCItem,
+  FindMatch,
+  ChatMessage,
+  AIRewriteOptions,
+  GeneratedCell,
+  AICellGenerateOptions,
+} from "./components";
