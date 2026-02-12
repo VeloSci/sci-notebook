60e37f04163f49acc844d172ec991fb3987f0f9b
Jose Ivan <jigonzalez930209@gmail.com>
2026-02-11 22:41:43 -0300

    chore(vue): update public exports

--- packages/vue/src/index.ts
+++ packages/vue/src/index.ts
@@ -2,6 +2,30 @@ export { SciNotebook } from "./SciNotebook";
 export type { SciNotebookProps } from "./SciNotebook";
 export { NotebookCell } from "./NotebookCell";
 export { InsertHandle } from "./InsertHandle";
+export { FloatingToolbar } from "./FloatingToolbar";
+export { MathEditor, MATH_CATEGORIES } from "./MathEditor";
+export type { MathBlock, MathCategory } from "./math-categories";
+export { ImageCell, renderImagePreview } from "./ImageCell";
+export { EmbedCell, renderEmbedPreview } from "./EmbedCell";
+export { SlashCommand, DEFAULT_COMMANDS } from "./SlashCommand";
+export type { SlashCommandItem } from "./SlashCommand";
+export { TableCell, renderTablePreview } from "./TableCell";
+export { TOCSidebar } from "./TOCSidebar";
+export type { TOCItem } from "./TOCSidebar";
+export { FindReplace } from "./FindReplace";
+export type { FindMatch } from "./FindReplace";
+export { LatexAutocomplete, LATEX_COMMANDS } from "./LatexAutocomplete";
+export { CellOutputDisplay } from "./CellOutput";
+export { GhostText } from "./GhostText";
+export { ChatSidebar } from "./ChatSidebar";
+export type { ChatMessage, ChatSidebarProps } from "./ChatSidebar";
+export { ImageResize } from "./ImageResize";
+export { VirtualRenderer } from "./VirtualRenderer";
+export { MermaidPreview, initMermaid } from "./MermaidCell";
+export { AIRewrite } from "./AIRewrite";
+export type { AIRewriteProps } from "./AIRewrite";
+export { AICellGenerate } from "./AICellGenerate";
+export type { GeneratedCell, AICellGenerateProps } from "./AICellGenerate";
 export {
   useNotebookEngine,
   provideNotebookEngine,
