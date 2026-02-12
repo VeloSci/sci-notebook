40afdb6f2d8762a125f32617c097ba67bc2358aa
Jose Ivan <jigonzalez930209@gmail.com>
2026-02-11 22:37:49 -0300

    chore(vanilla): export public API

--- packages/vanilla/src/index.ts
+++ packages/vanilla/src/index.ts
@@ -3,3 +3,36 @@ export type { SciNotebookVanillaOptions, VanillaCellRenderer } from "./sci-noteb
 export { DOMCellRenderer } from "./dom-cell-renderer";
 export { DragDropManager } from "./drag-drop-manager";
 export { KeyboardHandler } from "./keyboard-handler";
+export { MATH_CATEGORIES } from "./math-categories";
+export type { MathBlock, MathCategory } from "./math-categories";
+export {
+  FloatingToolbar,
+  SlashCommandMenu,
+  DEFAULT_COMMANDS,
+  buildTOCItems,
+  createTOCSidebar,
+  FindReplaceBar,
+  renderCellOutput,
+  renderCellOutputs,
+  renderImagePreview,
+  renderEmbedPreview,
+  renderTablePreview,
+  renderMermaidToSvg,
+  initMermaid,
+  LatexAutocompleteMenu,
+  LATEX_COMMANDS,
+  ChatSidebarPanel,
+  AIRewritePanel,
+  AICellGeneratePanel,
+  GhostTextOverlay,
+  ImageResizeHandle,
+} from "./components";
+export type {
+  SlashCommandItem,
+  TOCItem,
+  FindMatch,
+  ChatMessage,
+  AIRewriteOptions,
+  GeneratedCell,
+  AICellGenerateOptions,
+} from "./components";
